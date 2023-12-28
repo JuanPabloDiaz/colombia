669a4b01d76952fdd0857c993f8f9f7d3b929515
JuanPabloDiaz <juanchodis@hotmail.com>
2023-12-28 10:59:33 -0500

    Add layout and hero image to App component based on the figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import Layout from "./Components/Layout.jsx";
+import hero from "./Assets/hero.png";
+import frame from "./Assets/hero-frame.png";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
+      <Layout>
+        <div className="flex flex-col relative">
+          <img src={frame} alt="hero" className="w-[700px] absolute" />
+          <img src={hero} alt="hero" className="w-[1000px]" />
+        </div>
+      </Layout>
     </>
   );
 }
