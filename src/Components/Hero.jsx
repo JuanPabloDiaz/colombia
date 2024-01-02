98458094f4cb203cbae9ae5ef83e0faac75807a7
JuanPabloDiaz <juanchodis@hotmail.com>
2024-01-02 13:21:02 -0500

    Remove unused code for hamburger menu icon and close menu icon

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -57,24 +57,6 @@ export default function Hero() {
           className="flex items-center justify-end p-12 md:px-10 lg:px-20"
           aria-label="Global"
         >
-          {/* Hamburger menu icon: */}
-          {/* Set to hidden since its not functional yet. it taking the entire screen and wont close after click. (To show change hidden to flex in small screens. Also need to change the md to lg on the classNamesNavBarScroll ^^ */}
-          <div
-            className={classNamesNavBarScroll(
-              scrollPosition > 0 ? "bg-[#132577]" : "bg-none",
-              "hidden lg:hidden fixed p-2 rounded-full transition-color duration-1000"
-            )}
-          >
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white hover:text-gray-400"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          {/* Table & Desktop Navigation: */}
           <div className="hidden md:flex md:gap-x-8 lg:gap-x-12 md:justify-end">
             {navigation.map((item) => (
               <Link
@@ -97,17 +79,6 @@ export default function Hero() {
         >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            {/* X to Close menu icon: */}
-            <div className="flex items-center justify-end">
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-white  hover:text-gray-400"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
