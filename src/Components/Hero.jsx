939960736f11af0393392a88a058199a5c1667ab
JuanPabloDiaz <juanchodis@hotmail.com>
2024-01-02 16:06:55 -0500

    Update Hero component imports and styles

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@
 import frame from "../assets/hero-frame.png";
 import backgroundImg from "../assets/banner-background1440.png";
 import contactImage from "../assets/contact-us.png";
-import service from "../assets/img/16-face50.png";
+import { PiDrop } from "react-icons/pi";
 import NavBar from "./Navbar";
 
 export default function Hero() {
@@ -18,7 +18,7 @@ export default function Hero() {
     >
       <div className="absolute inset-0 z-0">
         <img
-          className="w-full object-cover md:w-11/12 lg:w-1/2"
+          className="w-full object-cover md:w-11/12 lg:w-8/12"
           src={frame}
           alt="Hero"
         />
@@ -34,28 +34,28 @@ export default function Hero() {
                 <h1 className="text-3xl font-bold tracking-tight text-[#2E8CAD] sm:text-5xl">
                   Talent For AI
                 </h1>
-                <p className="mt-12 text-lg leading-8 text-black">
+                <p className="mt-12 w-96 text-justify text-lg leading-8 text-black">
                   Revolutionize your customer experience with our AI chatbot,
                   offering natural language understanding, personalized
                   recommendations, and seamless purchasing. Our experts will
                   elevate your customer interactions.
                 </p>
-                <ul className="leading-2 mt-4 text-lg text-black">
+                <ul className="leading-2 mt-4 text-lg font-semibold text-[#2E8CAD]">
                   <a href="javascript:void()">
                     <li className="flex items-center justify-start gap-2">
-                      <img src={service} alt="img" className="h-5 w-5" />
+                      <PiDrop />
                       <span>services 1</span>
                     </li>
                   </a>
                   <a href="javascript:void()">
                     <li className="flex items-center justify-start gap-2">
-                      <img src={service} alt="img" className="h-5 w-5" />
+                      <PiDrop />
                       <span>services 2</span>
                     </li>
                   </a>
                   <a href="javascript:void()">
                     <li className="flex items-center justify-start gap-2">
-                      <img src={service} alt="img" className="h-5 w-5" />
+                      <PiDrop />
                       <span>services 3</span>
                     </li>
                   </a>
@@ -63,9 +63,9 @@ export default function Hero() {
                 <div className="justify-left mt-10 flex items-center gap-x-6">
                   <a
                     href="javascript:void()"
-                    className="hover:bg-white-500 mt-32 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-[#92989f] hover:text-[#132577] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="hover:bg-white-500 rounded-full border border-2 border-[#95D4EB] bg-[#95D4EB] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white hover:text-[#95D4EB] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    EXPLORE MORE
+                    I need help
                   </a>
                 </div>
               </div>
